33c4dd7ad5d5c0e555e750f00ea116fa3a396f45
krga <krga@itu.dk>
2021-10-28 13:47:49 +0200

    add_generateInput2

--- HashQualityEvaluation.java
+++ HashQualityEvaluation.java
@@ -1,9 +1,14 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class HashQualityEvaluation {
 
@@ -13,16 +18,24 @@ public class HashQualityEvaluation {
         return arr;
     }
 
-    
-    
+    static int[] generateInput2(int n) {
+        int seed = 42;
+        Set<Integer> elements = new HashSet<>();
+        while (elements.size() != n) {
+            int randInt = ThreadLocalRandom.current().nextInt(1, seed);
+            elements.add(randInt);
+        }
+        return elements.stream().mapToInt(i -> i).toArray();
+    }
+
     public static void main(String[] args) {
-        
+
         int[] arr = new Random().ints(1000000, 1, 1000000).map(i -> HyperLogLog.h(i)).toArray();
-        
+
         HashMap<Integer, Integer> p_distribution = new HashMap<>();
-        
+
         for (int i = 0; i < arr.length; i++) {
-            int key = HyperLogLog.p(arr[i]); // get p-value 
+            int key = HyperLogLog.p(arr[i]); // get p-value
             if (p_distribution.containsKey(key)) {
                 int count = p_distribution.get(key);
                 count++;
@@ -42,17 +55,15 @@ public class HashQualityEvaluation {
         /**
          * Write results to file
          */
-        
+
         String eol = System.getProperty("line.separator");
-    
+
         try (Writer writer = new FileWriter("hash_quality_results.csv")) {
             writer.append("p-value,frequency").append(eol);
             for (Map.Entry<Integer, Integer> entry : p_distribution.entrySet()) {
-            writer.append(Integer.toString(entry.getKey()))
-              .append(',')
-              .append(Integer.toString(entry.getValue()))
-              .append(eol);
-        }
+                writer.append(Integer.toString(entry.getKey())).append(',').append(Integer.toString(entry.getValue()))
+                        .append(eol);
+            }
         } catch (IOException ex) {
             ex.printStackTrace(System.err);
         }
